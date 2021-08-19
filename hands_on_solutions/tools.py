a74cc1485776029f5764aec09ec1c9f48dafad0a
Pietro Berkes <pietro.berkes@googlemail.com>
2021-08-19 09:20:41 +0200

    `imshow` is not defined and not needed in `tools.py`

--- hands_on_solutions/tools.py
+++ hands_on_solutions/tools.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2018  Nicolas P. Rougier
 # Distributed under the terms of the BSD License.
 # -----------------------------------------------------------------------------
-from imshow import imshow
+
 
 def sysinfo():
     import sys
